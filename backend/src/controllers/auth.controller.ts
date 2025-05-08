e045c3b7f39ada45d6f10d7da9e85a2ae93f6473
Taro Altricher <altrichter.taro@outlook.com>
2025-05-08 21:05:30 +0200

    Backend: Login and Logout functionality

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -55,3 +55,47 @@ export const signup = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Internal Server Error." });
     }
 };
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { username } });
+
+        if (!user) {
+            res.status(400).json({ error: "Invalid username or password." });
+            return;
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) {
+            res.status(400).json({ error: "Invalid username or password." });
+            return;
+        }
+
+        generateToken(user.id, res);
+
+        res.status(200).json({
+            id: user.id,
+            username: user.username,
+            fullname: user.fullname,
+            profilePicture: user.profilePicture,
+        });
+    } catch (error: any) {
+        console.log("Error in Login Controller.", error.message);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully." });
+    } catch (error: any) {
+        console.log("Error in Logout Controller.", error.message);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
