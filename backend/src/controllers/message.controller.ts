983e432b0b93870715b6b41a3a696b996bbc1072
Taro Altricher <altrichter.taro@outlook.com>
2025-05-08 23:24:30 +0200

    Backend: Send Message Functionality

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import prisma from "../db/prisma";
+
+export const sendMessage = async (req: Request, res: Response) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user.id;
+
+        let chat = await prisma.chat.findFirst({
+            where: {
+                participantIds: {
+                    hasEvery: [senderId, receiverId],
+                },
+            },
+        });
+
+        if (!chat) {
+            chat = await prisma.chat.create({
+                data: {
+                    participantIds: {
+                        set: [senderId, receiverId],
+                    },
+                },
+            });
+        }
+
+        const newMessage = await prisma.message.create({
+            data: { senderId, body: message, chatId: chat.id },
+        });
+
+        if (newMessage) {
+            chat = await prisma.chat.update({
+                where: {
+                    id: chat.id,
+                },
+                data: {
+                    messages: {
+                        connect: {
+                            id: newMessage.id,
+                        },
+                    },
+                },
+            });
+        }
+
+        res.status(201).json(newMessage);
+    } catch (error: any) {
+        console.error("Error in Send Message Controller.", error.message);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT}`);
