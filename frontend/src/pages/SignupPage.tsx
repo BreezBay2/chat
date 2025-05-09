e47a132a690f6e9ff1d8f37d66a3836fe8ab8999
Taro Altricher <altrichter.taro@outlook.com>
2025-05-09 20:46:09 +0200

    Frontend: Signup Page styling

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useSignup from "../hooks/useSignup";
+import "../styles/SignupPage.css";
 
 const SignupPage = () => {
     const [signupForm, setSignupForm] = useState({
@@ -18,58 +19,64 @@ const SignupPage = () => {
     };
 
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmitForm}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={signupForm.username}
-                    onChange={(e) =>
-                        setSignupForm({
-                            ...signupForm,
-                            username: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    type="text"
-                    placeholder="Your Name"
-                    value={signupForm.fullname}
-                    onChange={(e) =>
-                        setSignupForm({
-                            ...signupForm,
-                            fullname: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={signupForm.password}
-                    onChange={(e) =>
-                        setSignupForm({
-                            ...signupForm,
-                            password: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    value={signupForm.confirmPassword}
-                    onChange={(e) =>
-                        setSignupForm({
-                            ...signupForm,
-                            confirmPassword: e.target.value,
-                        })
-                    }
-                />
-                <Link to={"/login"}>Already have an account?</Link>
-                <button disabled={loading}>
-                    {loading ? "Loading..." : "Sign Up"}
-                </button>
-            </form>
+        <div className="signup-page">
+            <div className="signup-container">
+                <h1>Sign Up</h1>
+                <form className="signup-form" onSubmit={handleSubmitForm}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={signupForm.username}
+                        onChange={(e) =>
+                            setSignupForm({
+                                ...signupForm,
+                                username: e.target.value,
+                            })
+                        }
+                    />
+                    <input
+                        type="text"
+                        placeholder="Your Name"
+                        value={signupForm.fullname}
+                        onChange={(e) =>
+                            setSignupForm({
+                                ...signupForm,
+                                fullname: e.target.value,
+                            })
+                        }
+                    />
+                    <div className="signup-passwords">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={signupForm.password}
+                            onChange={(e) =>
+                                setSignupForm({
+                                    ...signupForm,
+                                    password: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            value={signupForm.confirmPassword}
+                            onChange={(e) =>
+                                setSignupForm({
+                                    ...signupForm,
+                                    confirmPassword: e.target.value,
+                                })
+                            }
+                        />
+                    </div>
+                    <Link className="login-link" to={"/login"}>
+                        Already have an account?
+                    </Link>
+                    <button disabled={loading}>
+                        {loading ? "Loading..." : "Sign Up"}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
