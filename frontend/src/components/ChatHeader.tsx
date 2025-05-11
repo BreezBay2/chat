07f6ef8626a2907fca99ea72c3ed8549e9958cab
Taro Altricher <altrichter.taro@outlook.com>
2025-05-11 15:15:08 +0200

    Frontend: Online Indicator in Chat Header

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -1,9 +1,14 @@
 import { X } from "@phosphor-icons/react";
 import "../styles/components/ChatHeader.css";
 import useChatStore from "../zustand/useChatStore";
+import { useSocketContext } from "../context/SocketContext";
 
 const ChatHeader = () => {
     const { selectedUser, setSelectedUser } = useChatStore();
+    const { onlineUsers } = useSocketContext();
+    const isOnline = selectedUser
+        ? onlineUsers.includes(selectedUser.id)
+        : null;
 
     return (
         <div className="header-container">
@@ -11,7 +16,16 @@ const ChatHeader = () => {
                 <img src={selectedUser?.profilePicture} />
                 <div className="header-names">
                     <p>{selectedUser?.fullname}</p>
-                    <p className="header-online-status">Offline</p>
+                    <div>
+                        {isOnline ? (
+                            <div className="header-online-status">
+                                <div className="header-online-icon"></div>
+                                <p>Online</p>
+                            </div>
+                        ) : (
+                            <p className="header-offline-status">Offline</p>
+                        )}
+                    </div>
                 </div>
             </div>
             <X
